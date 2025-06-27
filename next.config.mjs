2cb1ca74ad1b071b83e56aa94eece6b6e442c007
JT-Production <joeyofficial707@gmail.com>
2025-06-27 10:02:12 +0100

    remove cache

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+ 
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  compress: true,
+};
 
-export default nextConfig
+export default nextConfig;
